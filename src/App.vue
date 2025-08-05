b2f45c756b11b13ab03a56c9bf46b4139ecd14be
matthewfinis <matthew.finis@gmail.com>
2025-08-05 17:29:48 +1000

    week 2 lab

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Json from './components/JSON.vue'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <Json />
+  </main>
+</template>
+
+<style scoped>
+</style>
